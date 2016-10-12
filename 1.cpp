7ddacc2d1c8080716535ea032ec89a6057b948c6
parksoeun <thdms3549@naver.com>
2016-10-12 14:36:05 +0900

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,1 @@
+#include <iostream>#include <string>template <class T_CLASS>void print(const T_CLASS& i){ std::cout << i << std::endl;}int main() { print(1); print(2.345f); print("Hello World"); return 0;}
